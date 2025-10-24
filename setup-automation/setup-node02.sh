cbdffbfe8e90f8cb5c9a917be068e4f29d17409e
Nuno Martins <nmartins@redhat.com>
2025-10-24 10:12:26 +0200

    Update setup-node02.sh

--- setup-automation/setup-node02.sh
+++ setup-automation/setup-node02.sh
@@ -21,7 +21,7 @@ subscription-manager status
 if [ $? -ne 0 ]; then
     retry "subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}"
 fi
-retry "install nano subscription-manager kbd insights-client  -y"
+retry "dnf install nano subscription-manager kbd insights-client  -y"
 
 
 # subscription-manager unregister
