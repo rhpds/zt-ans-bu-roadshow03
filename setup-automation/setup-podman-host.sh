9951c6d506edbcd25ac842089890010baf7e3ba0
Nuno Martins <nmartins@redhat.com>
2025-10-24 09:22:18 +0200

    Update setup-podman-host.sh

--- setup-automation/setup-podman-host.sh
+++ setup-automation/setup-podman-host.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
-curl -k  -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt
-update-ca-trust
-rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm
 
-subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}
+retry() {
+    for i in {1..3}; do
+        echo "Attempt $i: $2"
+        if $1; then
+            return 0
+        fi
+        [ $i -lt 3 ] && sleep 5
+    done
+    echo "Failed after 3 attempts: $2"
+    exit 1
+}
 
-yum install yum-utils jq podman wget git ansible-core nano -y
+retry "curl -k -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt"
+retry "update-ca-trust"
+KATELLO_INSTALLED=$(rpm -qa | grep -c katello)
+if [ $KATELLO_INSTALLED -eq 0 ]; then
+  retry "rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm"
+fi
+subscription-manager status
+if [ $? -ne 0 ]; then
+    retry "subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}"
+fi
+retry "install yum-utils jq podman wget git ansible-core nano -y"
+
 setenforce 0
 firewall-cmd --permanent --add-port=2000:2003/tcp
 firewall-cmd --permanent --add-port=6030:6033/tcp
